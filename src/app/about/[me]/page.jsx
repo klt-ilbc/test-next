f9aab5a445cb391db99e725c4fb9233128d70285
kyawlinnthant <kyawlinnthant@ilbcedu.com>
2023-08-19 01:36:00 +0630

    ssr

--- src/app/about/[me]/page.jsx
+++ src/app/about/[me]/page.jsx
@@ -1,7 +1,10 @@
-"use client";
 const Me = ({ params }) => {
-  console.log(params);
-  return <div className="text-white font-2xl">Me</div>;
+  console.log(params, "from post");
+  return (
+    <div className="text-white flex justify-center items-center min-h-screen text-3xl font-2xl">
+      {params.me}
+    </div>
+  );
 };
 
 export default Me;
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
-export default function Home() {
+export default async function Home() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts/1", {
+    cache: "no-store",
+  });
+  const data = await res.json();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-white text-3xl">Hello Amplify</h1>
+      <p className="text-white text-md">{JSON.stringify(data)}</p>
     </main>
   );
 }
