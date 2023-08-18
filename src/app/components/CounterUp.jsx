e133ab1972e6699de55b26e60f0047a9c250a3f6
kyawlinnthant <kyawlinnthant@ilbcedu.com>
2023-08-18 21:21:55 +0630

    amplify

--- src/app/components/CounterUp.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import CountUp from "react-countup";
-
-export default function CounterUp({ endVal }) {
-  return <CountUp end={endVal} />;
-}
--- src/app/components/profile/setting/Courses.jsx
+++
@@ -1,8 +0,0 @@
-import { Stack } from "@mui/material";
-import React from "react";
-
-const Courses = () => {
-  return <Stack direction="column"></Stack>;
-};
-
-export default Courses;
