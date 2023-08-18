cd8ba1eb0f81041fd62f76066ed83f74e0955c47
kyawlinnthant <kyawlinnthant@ilbcedu.com>
2023-08-18 21:53:30 +0630

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
+  console.log(process.env.NODE_ENV);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="font-mono text-white text-3xl">Hello Amplify</h1>
+      <p className="text-white">{process.env.NEXTAUTH_UR}</p>
     </main>
   );
 }
