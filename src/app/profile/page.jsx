02071197501a1870d6d2d57fdbde299c2b22c56a
kyawlinnthant <kyawlinnthant@ilbcedu.com>
2023-08-19 02:04:56 +0630

    status

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,7 +1,7 @@
 const page = () => {
   return (
     <div className="text-white flex justify-center items-center min-h-screen text-3xl font-2xl">
-      Main Profile
+      Main Profile HELLO
     </div>
   );
 };
