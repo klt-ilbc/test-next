7f854405f9649caa54064e38356df335e35eedca
kyawlinnthant <kyawlinnthant@ilbcedu.com>
2023-08-18 22:15:46 +0630

    env

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ export default function Home() {
   console.log(process.env.googleClientId);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="font-mono text-white text-3xl">Hello Amplify</h1>
       <h1 className="font-mono text-white text-3xl">
         {process.env.googleClientId}
       </h1>
